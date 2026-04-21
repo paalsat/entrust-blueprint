dfb2f9a64da10eef3f34b31e9fd1c86719ed35ab
paalsat <paalsat@gmail.com>
2026-04-21 16:22:08 +0200

    Update conf.py
    Fixed project details.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,12 +2,13 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'EOSC-ENTRUST Blueprint & Interoperability Framework'
+copyright = ''
+html_show_copyright = False
+author = 'The Contributors'
 
-release = '0.1'
-version = '0.1.0'
+release = '0.0'
+version = '0.0.0'
 
 # -- General configuration
 
