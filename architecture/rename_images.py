4b01e90984582d134ba65be88f7e983350535fc4
paalsat <paalsat@gmail.com>
2026-04-23 16:09:32 +0200

    Update rename_images.py

--- architecture/rename_images.py
+++ architecture/rename_images.py
@@ -1,7 +1,6 @@
 import os
 import re
 import sys
-import shutil
 from bs4 import BeautifulSoup
 
 _report_path = "./architecture/.report"
@@ -28,10 +27,8 @@ def rename_images(report_path):
                     new_image = os.path.join(images_path, f"{clean_name}.png")
 
                     if os.path.exists(old_image):
-                        print(f"Copying {view_id} -> {clean_name}")
-                        shutil.copy(old_image, new_image)
-                        #print(f"Renaming {view_id} -> {clean_name}")
-                        #os.rename(old_image, new_image)
+                        print(f"Renaming {view_id} -> {clean_name}")
+                        os.rename(old_image, new_image)
 
 if __name__ == "__main__":
     report_path = _report_path if len(sys.argv) == 1 else sys.argv[1]
